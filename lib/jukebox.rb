6e37053cdd19dd8a0f26c5b4cbb29ad75d76f9ef
Paul Ashour <paul.ashour@gmail.com>
2018-10-09 22:14:12 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,6 +14,10 @@ def help
   puts /^(?=.*help) (?=.*list) (?=.*play) (?=.*exit) .+/m
 end
 
+def input_error
+  puts 'Invalid input, please try again'
+end
+
 def play(songs)
   play_command = gets.chomp
   if play_command =~ /\A\d+\Z/
@@ -22,14 +26,14 @@ def play(songs)
     if play_command.between?(1, songs.length) # check that number is valid
       puts songs[play_command.to_i - 1]
     else
-      puts 'Invalid input, please try again'
+      input_error
     end
   else
     # not a number, check for song title match
     if songs.find { |song| song == play_command }
       puts play_command
     else
-      puts 'invalid entry'
+      input_error
     end
   end
     
